2ac57e69c39bc3d434495b1e403d5e8d1831eab5
Bruno da Gama Porciuncula <bruno.porciuncula-ext@viavare.com.br>
2020-09-07 15:52:19 -0300

    added restore-missing-simulators.sh

---
+++ restore-missing-simulators.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Reason:
+# I have some bug on my system which makes all installed simulators on my Xcode missing.
+# This will reset `Xcode` and `CoreSimulatorService` and make simulators re-appear in the Xcode list
+
+osascript -e 'do shell script "sudo pkill -9 -fi Xcode CoreSimulatorService" with administrator privileges'
