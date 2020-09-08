27af7a6e10ced7eb033b2e415ea58c1c751ecf20
Bruno da Gama Porciuncula <bruno.porciuncula-ext@viavare.com.br>
2020-09-08 10:59:41 -0300

    new scripts

---
+++ remove-derived-data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -frd ~/Library/Caches/com.apple.dt.Xcode/*
---
+++ stash-pull-pop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ${SRCROOT}
+git stash && git pull && git pop
