b409cf97b44b40e4bfb80b7e3a84ae93dd8c5953
Bruno Porciuncula <bruno.porciuncula@bancointer.com.br>
2022-03-29 09:08:19 -0300

    added new scripts

---
+++ open-iterm-down-2-directories.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Project Name:  $XcodeProject
+# Project Dir:   $XcodeProjectPath
+# Workspace Dir: $XcodeWorkspacePath
+
+open -a Iterm "$(dirname ${XcodeProjectPath})../.."
